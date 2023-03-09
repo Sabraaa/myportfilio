e555730ebff656bc7b303e4952d8c29f1575a9fc
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-03-09 20:10:51 +0330

    secondtoday

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,32 @@
 import React from "react";
-import "./Header.scss"
+import "./Header.scss";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faSolid} from '@fortawesome/fontawesome-svg-core/import.macro'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <div>
-      <div className="container">
-        <h1> Sabra</h1>
-        <div className="right-side">
+      <div className="main-container">
+    <div className="container">
 
-          <div className="titles">
+        <h1> Sabra</h1>
+        <div className="container-right">
+          <div className="container-right-title">
             <h6> Home </h6>
             <h6> About </h6>
             <h6> Project </h6>
             <h6> Resume </h6>
           </div>
           <button className="btn">
-            <i className="fa-solid fa-sun-bright"></i>
+            <FontAwesomeIcon icon={faSun} />
+            {/* <i className="fa-solid fa-sun-bright"></i> */}
             {/* <FontAwesomeIcon icon={solid('sun-bright')} /> */}
           </button>
         </div>
-
+    </div>
       </div>
-
-
-
     </div>
   );
 }
