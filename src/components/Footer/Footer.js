57d92e31c7abb08c5ad27b85589509823e9f4bcd
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-03-11 19:43:58 +0330

    third

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faSquareGithub, faLinkedin, faEnvelope, faInsragram } from "@fortawesome/free-solid-svg-icons";
+
+const Footer = () => {
+  return (
+    <div className="main-container">
+      <div className="container">
+      <div>
+      <FontAwesomeIcon icon={faSquareGitHub}/>
+      <FontAwesomeIcon icon={faLinkedin}/>
+      <FontAwesomeIcon icon={faEnvelope}/>
+      <FontAwesomeIcon icon={faInsragram}/>
+      </div>
+        <p>
+          Designed and coded by Sabra
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
