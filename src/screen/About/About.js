9263ebbe3b5b7f31ef0a96c98e54a794d35cef1d
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-03-14 01:17:44 +0330

    4th 2

---
+++ src/screen/About/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="top-about-container">
+        <div className="top-about-title-one"></div>
+        <div className="top-about-title-two"></div>
+        <div className="top-about-prgrph"></div>
+      </div>
+      <div className="bottom-about-container">
+        <div className="bottom-about-title"></div>
+        <div className="bottom-about-icons"></div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
