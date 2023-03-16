4fb18662e8a494b251db3e8b71f7e15dbe0335d8
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-03-16 18:47:41 +0330

    funf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-
+import About from "./screen/About/About";
+import Home from "./components/Home/Home"
 
 function App() {
   return (
     <div>
-  <Header/>
-  <p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a
-
-  </p>
-  <Footer/>
+      <Header />
+      <Home/>
+      {/* <About /> */}
+      <Footer />
     </div>
   );
 }
--- src/screen/About/About.js
+++ src/screen/About/About.js
@@ -5,12 +5,18 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="top-about-container">
-        <div className="top-about-title-one"></div>
-        <div className="top-about-title-two"></div>
-        <div className="top-about-prgrph"></div>
+        <div className="top-about-title-one"> About me</div>
+        <div className="top-about-title-two"> Here is my story;</div>
+        <div className="top-about-prgrph"> Hi, I'm Sabra Akhavan. a front-end web developer from Iran. I enjoy seeking out creative solutions to complex problems. It was back in late 2021 when I got interested in Web Development and decided to learn HTML & CSS. Fast forward to today, I learned various web technologies and their best practices and built projects.
+
+I love programming because it is so practical, and I can solve real-world challenges with it. For me, programming is a tool to express myself in a creative way. Plus, there is so much to explore that always keeps me excited!
+<br/>
+<br/>
+In my free time, I get inspiration from other people's work and try out new technologies to build side projects. Apart from that, I also read books, Paint, watch movies, practice Adobes and so on.
+</div>
       </div>
       <div className="bottom-about-container">
-        <div className="bottom-about-title"></div>
+        <div className="bottom-about-title"> Skills & Tools </div>
         <div className="bottom-about-icons"></div>
       </div>
     </div>
