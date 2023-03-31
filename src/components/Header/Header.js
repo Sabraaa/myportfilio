651d338a2beeff16c3a317db5924ce4a59fa33e9
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-03-31 11:15:39 +0330

    zehnten end

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { Link, Outlet } from "react-router-dom";
-import RSM from "../../assets/sabracodes.pdf"
+import RSM from "../../assets/sabracodes.pdf";
 
 function Header() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <div>
-      <div className="main-header-container">
+      <div className={`main-header-container ${theme}`}>
         <div className="header-container">
-          <h1> Sabra</h1>
+          <div className="header-profile-name"> Sabra</div>
           <div className="header-container-right">
-            <div className="header-container-right-title">
-        
-              <header>
-      <nav className="header-container-right-title">
-        
-          <Link  className="header-link" to="/">Home </Link>
-          <Link  className="header-link" to="/about">About </Link>
-          <Link  className="header-link" to="/projects">Projects </Link>
-          <Link className="header-link" to="/resume" onClick={() => window.open(`${RSM}`, "_blank")}>Resume</Link>
-
-      </nav>
-    </header>
+            <div>
+              <nav className="header-container-right-title">
+                <Link className="header-link" to="/">
+                  Home
+                </Link>
+                <Link className="header-link" to="/about">
+                  About
+                </Link>
+                <Link className="header-link" to="/projects">
+                  Projects
+                </Link>
+                <Link
+                  className="header-link"
+                  to="/resume"
+                  onClick={() => window.open(`${RSM}`, "_blank")}
+                >
+                  Resume
+                </Link>
+              </nav>
             </div>
-            <button className="btn">
-              <FontAwesomeIcon icon={faSun} />
-            </button>
+              {/* <button className="btn">
+                <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} />
+              </button> */}
           </div>
         </div>
       </div>
